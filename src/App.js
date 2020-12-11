bfd44419ec1ecc9bfb5aeb769403a6fd0f8df68f
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-12-11 00:01:00 -0300

    feat: Centralize the main content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 import GlobalStyles from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
+
+const Content = styled.main`
+  margin: 1rem auto 0 auto;
+  max-width: 700px;
+  width: 100%;
+`;
 
 function App() {
   return (
@@ -11,7 +18,9 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Header />
-        <Routes />
+        <Content>
+          <Routes />
+        </Content>
       </BrowserRouter>
     </div>
   );
