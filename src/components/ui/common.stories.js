457635ddab3a2a2e42577a78171459a0aded43fc
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-12-10 21:51:59 -0300

    feat: Create common ui Card

---
+++ src/components/ui/common.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Card } from './index';
+
+export default {
+  title: 'Common/ui',
+};
+
+export const card = () => <Card>Card</Card>;
---
+++ src/components/ui/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  padding: 0.5rem;
+  margin: 0 0.5rem 0.5rem 0.5rem;
+
+  background: var(--boundary);
+
+  border: none;
+  border-radius: 5px;
+`;
