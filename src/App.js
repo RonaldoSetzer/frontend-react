7d2765d1d93d0a9c8afac1a591482559ffe09388
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-12-10 23:06:51 -0300

    feat: Implements Header to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <GlobalStyles />
       <BrowserRouter>
+        <Header />
         <Routes />
       </BrowserRouter>
     </div>
