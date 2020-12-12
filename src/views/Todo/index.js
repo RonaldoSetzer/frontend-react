2ff2d710590bc4e4e10eb84122aefad68b6d3476
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-12-11 22:58:05 -0300

    feat: Create the sketch of Todo view

--- src/views/Todo/index.js
+++ src/views/Todo/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import InputForm from '../../components/InputForm';
+import TodoList from '../../components/TodoList';
+
 function Todo() {
-  return <h1>Todo</h1>;
+  const todos = ['todo 01', 'todo 02', 'todo 03', 'todo 04', 'todo 05'];
+  const totalCompleted = 3;
+
+  function handleSubmit(value) {
+    console.log(value);
+  }
+
+  return (
+    <>
+      <InputForm placeholder="ADD TO DO" handleSubmit={handleSubmit} />
+      <TodoList list={todos} totalCompleted={totalCompleted} />
+    </>
+  );
 }
 
 export default Todo;
