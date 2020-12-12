899903df2a0e1215a9f409064d94590e384cb34e
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-12-12 14:29:27 -0300

    feat: Create Weather view

--- src/views/Weather/index.js
+++ src/views/Weather/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import InputForm from '../../components/InputForm';
+import WeatherCard from '../../components/WeatherCard';
 import { ViewContainer } from '../../components/ui';
 
 function Weather() {
+  function handleSubmit(value) {
+    console.log(value);
+  }
+
   return (
     <ViewContainer>
-      <h1>Weather</h1>
+      <InputForm placeholder="TYPE A CITE" handleSubmit={handleSubmit} />
+      <WeatherCard day="TODAY" temperature="38.5C" />
+      <WeatherCard day="TOMORROW" temperature="38.5C" />
+      <WeatherCard day="AFTER TOMORROW" temperature="38.5C" />
     </ViewContainer>
   );
 }
